ecb45974ca3fc1ad19c8be4eb9a8810120604e95
Chris Bolas <tanlaan33@gmail.com>
2021-02-25 22:15:11 -0800

    Initial commit

---
+++ spec/substring_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require_relative "../substring.rb"
+
+RSpec.describe "Sub Strings Exercise" do
+
+  describe '' do
+    it '' do
+      expect(1).to eq(1)
+    end
+  end
+end
+
---
+++ substring.rb
@@ -0,0 +1,3 @@
+def substring(phrase, dictionary)
+
+end
